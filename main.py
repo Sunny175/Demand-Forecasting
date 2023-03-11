7fd856c37d617d5b109e8ed592eeec590eb7f0a3
Sunny175 <sunnykadia421@gmail.com>
2023-03-11 10:07:33 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+import pickle
+
+
+app = Flask(__name__)
+
+
+#load the model
+model = pickle.load(open('gradientboostmodel.pkl', 'rb'))
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+    category = model.predict(request.form("category"))
+    cuisine = model.predict(request.form("cuisine"))
+    week = model.predict(request.form("week"))
+    check_out = model.predict(request.form("checkout_price"))
+    base_price = model.predict(request.form("base_price"))
+    emailer = model.predict(request.form("emailer"))
+    homepage = model.predict(request.form("homepage"))
+    city = model.predict(request.form("city"))
+    region = model.predict(request.form("region"))
+    op_area = model.predict(request.form("op_area"))
+    center_type = model.predict(request.form("center_type"))
+    return render_template('index.html')
+
+if __name__ ==  '__main__':
+    app.run(debug=True)
